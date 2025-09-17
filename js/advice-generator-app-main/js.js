21749c147d4f197637b71719ac5d3119a2083554
sohaila95952 <112872917+sohaila95952@users.noreply.github.com>
2025-09-17 21:37:18 +0300

    Add files via upload

---
+++ js/advice-generator-app-main/js.js
@@ -0,0 +1,16 @@
+﻿const adviceText = document.getElementById("adviceText");
+const adviceId = document.getElementById("adviceId");
+const diceBtn = document.getElementById("btnId");
+
+diceBtn.addEventListener("click", () => {
+    fetch("https://api.adviceslip.com/advice", { cache: "no-cache" })
+        .then(response => response.json())
+        .then(data => {
+            adviceId.textContent = data.slip.id;
+            adviceText.textContent = `"${data.slip.advice}"`;
+        })
+        .catch(error => {
+            adviceText.textContent = "Could not load advice";
+            console.error(error);
+        });
+});
